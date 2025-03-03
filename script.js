2b3ad0ec4c5c4f9720e0381e1994af5c58117f86
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-04 00:51:27 +0600

    infaniti with yoyo

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-gsap.to("#box",{
-    x: 1000,
-    duration:3,
-    rotate:360,
-    delay:0,
-    backgroundColor: "green",
-    borderRadius:"50%"
-})
+// gsap.from("#box",{
+//     x: 1000,
+//     duration:3,
+//     rotate:360,
+//     delay:0,
+//     backgroundColor: "green",
+//     borderRadius:"50%",
+//     scale:0.5,
+// })
 
 
 // gsap.from("#box",{
@@ -20,3 +21,26 @@ gsap.to("#box",{
 //     duration:5,
 // })
 
+
+
+// gsap.from("h1",{
+//     color: "red",
+//     duaration: 5,
+//     delay: 1,
+//     opacity: 0,
+//     x:-50,
+//     stagger: 1
+
+// })
+
+
+
+gsap.to("#box",{
+    x: 1300,
+    duaration: 5,
+    delay:1,
+    rotate: 350,
+    borderRadius:"50%",
+    repeat:-1,
+    yoyo:true
+})
