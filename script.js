0f0e6f07ca9193c760d6b26f7b86d08b6e145bc0
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-04 00:40:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.to("#box",{
+    x: 1000,
+    duration:3,
+    rotate:360,
+    delay:0,
+    backgroundColor: "green",
+    borderRadius:""
+})
+
+
+// gsap.from("#box",{
+//     y: 2000,
+//     duration:2,
+//     delay:3
+    
+// })
+
+// gsap.to("#box3",{
+//     x: 3000,
+//     duration:5,
+// })
+
