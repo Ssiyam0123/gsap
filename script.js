85fa207ca9248b97af2f03d8ac9ba1e1b24a5fd1
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-04 16:44:35 +0600

    custom cursor

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-var initialPath = `M 10 100 Q 500 100 990 100`
-var finalPath = `M 10 100 Q 500 100 990 100`
+var main = document.querySelector("#main")
 
-var string = document.querySelector("#string")
+var cursor = document.getElementById("#cursor")
+
+// main.addEventListener("mousemove", function(dets) {
+//     gsap.to(cursor, {
+//         x: dets.x,
+//         y: dets.y,
+//         duration: 0.2 // Corrected spelling of 'duration'
+//     });
+// });
 
 
-// string.addEventListener("mouseenter", function(dets){
-//     console.log(dets)
-//     console.log('enter')
-// })
 
-string.addEventListener("mousemove", function(dets){
-// console.log(dets.y)
-const path = `M 10 100 Q 500 ${dets.y} 990 100`
-
-gsap.to("svg path",{
-    attr: {d:path},
-    duaration: 0.3,
-    ease: "power3.out"
-
+main.addEventListener("click",function(){
+    console.log("event performed")
 })
-})
-
-
-
-
-string.addEventListener("mouseleave", function(){
-   gsap.to("svg path",{
-    attr:{d:finalPath},
-    duaration:0.3,
-    ease: "elastic.out(1,0.2)"
-   })
-})
-
-
-
-
-  
